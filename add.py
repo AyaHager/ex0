20a515e3a0f1e584da85a255d29da599e98b2432
AyaHager <hageraya@gmail.com>
2021-03-08 23:22:42 +0200

    add add file2

--- add.py
+++ add.py
@@ -5,6 +5,6 @@ Created on Mon Mar  8 23:16:02 2021
 @author: aya
 """
 
-val1 = input("insert number 1")
-val2 = input("insert number 2")
+val1 = int(input("insert number 1"))
+val2 = int (input("insert number 2"))
 print(val1 + val2)
